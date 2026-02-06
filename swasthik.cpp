749cf2c8076c5dd2b0d29b51514944d1fa0fd9b5
krishverma90399 <krishverma90399@gmail.com>
2026-02-06 13:27:59 +0530

    notes

---
+++ swasthik.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==4 || j==4 ||(i==1 || j>4) ||(j==n || i>4)||(i==n || j<4)||(j==1 || i<4)){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
+}
