62f8ad6d071491c595a1dae46c9ae41053295657
krishverma90399 <krishverma90399@gmail.com>
2026-02-24 15:31:20 +0530

    notes

--- merge_array.cpp
+++ merge_array.cpp
@@ -1,4 +1,8 @@
-int i=0,j=0;
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> merge(vector<int> a,vector<int> b){
+    int i=0,j=0;
     int n=a.size();
     int m=b.size();
     vector<int> nums;
@@ -21,3 +25,13 @@ int i=0,j=0;
         j++;
     }
     return nums;
+}
+int main() {
+    vector<int> a={1,3,6,9,10};
+    vector<int> b={2,4,5,7,8};
+    vector<int> merge_array=merge(a,b);
+    for(int i=0;i<merge_array.size();i++){
+        cout<<merge_array[i]<<" ";
+    }
+    return 0;
+}
