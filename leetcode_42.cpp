ecf23b818f5f1273a1d42506a0fb75aa2f7b1494
krishverma90399 <krishverma90399@gmail.com>
2026-02-25 13:31:16 +0530

    notes

---
+++ leetcode_42.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+    int left = 0 , right = n-1;
+    int answer = 0;
+    int rightmax = 0 , leftmax = 0;
+
+    while (left < right){
+
+        leftmax = max(leftmax , height[left]);
+        rightmax = max(rightmax , height[right]);
+
+        if(leftmax < rightmax){
+
+            answer += leftmax - height[left];
+            left++;
+        }
+        else{
+            answer += rightmax - height[right];
+            right--;
+        }       
+    }
+    return answer;
+    }
+};
