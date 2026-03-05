48531954ccc61daa7f9822127f443bd3300d0ec9
krishverma90399 <krishverma90399@gmail.com>
2026-03-05 23:31:46 +0530

    notes

---
+++ 2d_vector_array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> array(n,vector<int>(m));
+
+    return 0;
+}
+/*
+hw 
+*/
